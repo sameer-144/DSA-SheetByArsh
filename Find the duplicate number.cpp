5da91a257c57ad682aa834811735d9fcd9d48bdb
sameer-144 <105938869+sameer-144@users.noreply.github.com>
2022-05-21 18:36:27 +0530

    Create Find the duplicate number.cpp

---
+++ Find the duplicate number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow=nums[0],fast=nums[0];
+        
+        while(1)
+        {
+            slow=nums[slow];
+            fast=nums[nums[fast]];
+            if(fast==slow) break;
+        }
+        
+        slow=nums[0];
+        
+        while(slow!=fast)
+        {
+            slow=nums[slow];
+            fast=nums[fast];
+        }
+        
+        return slow;
+        
+    }
+};
